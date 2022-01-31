b6a21e72f61b12a4319219fa211470002090c448
Vasyl Hryts <vasylhryts74@gmail.com>
2022-01-31 16:26:03 +0200

    add handlebars

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
+const exphbs = require('express-handlebars');
 
 const app = express();
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
 async function start() {
     try {
