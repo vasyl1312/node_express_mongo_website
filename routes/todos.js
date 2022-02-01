b1100c318d95d30784fcec9a79cd25f6f7436c42
Vasyl Hryts <vasylhryts74@gmail.com>
2022-02-01 14:42:28 +0200

    add lighting to selected page

--- routes/todos.js
+++ routes/todos.js
@@ -2,11 +2,11 @@ const {Router}  = require('express')
 const router =  Router()
 
 router.get('/', (req, res) => {
-    res.render('index', {title: 'Home Page'})
+    res.render('index', {title: 'Home Page', isIndex: true})
 })
 
 router.get('/create', (req, res) => {
-    res.render('create', {title: 'Create Page'})
+    res.render('create', {title: 'Create Page', isCreate: true})
 })
 
 module.exports = router
