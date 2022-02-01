8f7d36183db59b3c0ae2cdd11d0c16d1691c7f19
Vasyl Hryts <vasylhryts74@gmail.com>
2022-02-01 14:35:46 +0200

    add good name for title

--- routes/todos.js
+++ routes/todos.js
@@ -2,11 +2,11 @@ const {Router}  = require('express')
 const router =  Router()
 
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {title: 'Home Page'})
 })
 
 router.get('/create', (req, res) => {
-    res.render('create')
+    res.render('create', {title: 'Create Page'})
 })
 
 module.exports = router
