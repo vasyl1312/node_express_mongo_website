6f145f385e45dfb7634ded147286cb05e398a617
Vasyl Hryts <vasylhryts74@gmail.com>
2022-02-02 21:30:15 +0200

    vladilen code

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const PORT = process.env.PORT || 3000;
-const exphbs = require('express-handlebars');
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const exphbs = require('express-handlebars')
 const todoRoutes = require('./routes/todos')
 
-const app = express();
+const PORT = process.env.PORT || 3000
 
+const app = express()
 const hbs = exphbs.create({
-    defaultLayout: 'main',
-    extname: 'hbs'
+  defaultLayout: 'main',
+  extname: 'hbs'
 })
 
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-app.set('views', 'views');
-app.use(todoRoutes);
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(todoRoutes)
 
 async function start() {
-    try {
+  try {
         await mongoose.connect('mongodb+srv://vasyl:Vasyl2002@cluster0.u5brf.mongodb.net/todos', {
-            // useNewUrlParser: true,
-            // useFindAndModify: false
+            useNewUrlParser: true,
+           // useFindAndModify: false
         })
-        app.listen(PORT, () => {
-            console.log('Server has been started...')
-        });
-    } catch (err) {
-        console.log(err);
+        
+    app.listen(PORT, () => {
+        console.log('Server has been started...')
+      })
+    } catch (e) {
+      console.log(e)
     }
-}
-
-start();
+  }
+  
+  start()
